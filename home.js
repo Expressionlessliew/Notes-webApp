22cec3c7edd8f423fcfa893c6f09e870ef636949
Kelveen Raj <kelveenrajv@gmail.com>
2023-06-11 14:44:48 +0800

    fixed

--- home.js
+++ home.js
@@ -1,3 +1,4 @@
+// Firebase authentication state change event listener
 firebase.auth().onAuthStateChanged(function (user) {
   if (user) {
     let displayName;
@@ -31,6 +32,9 @@ firebase.auth().onAuthStateChanged(function (user) {
     document.getElementById("userInfo").textContent = "";
     localStorage.removeItem("user");
   }
+
+  // Call loadTasks() here, after the authentication state is resolved
+  loadTasks();
 });
 
 // Logout button event listener
@@ -47,3 +51,72 @@ document.getElementById("logoutBtn").addEventListener("click", function () {
       console.log(error.message);
     });
 });
+
+const todoList = document.getElementById("todo-list");
+const newTaskInput = document.getElementById("new-task-input");
+const addTaskBtn = document.getElementById("add-task-btn");
+const clearAllBtn = document.getElementById("clear-all-btn");
+
+// Load tasks from local storage
+function loadTasks() {
+  const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  tasks.forEach((task) => createTaskItem(task));
+}
+
+// Save tasks to local storage
+function saveTasks(tasks) {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+// Create a new task item
+function createTaskItem(taskText) {
+  const taskItem = document.createElement("li");
+  taskItem.textContent = taskText;
+
+  // Create a clear button
+  const clearBtn = document.createElement("button");
+  clearBtn.textContent = "Clear";
+  clearBtn.addEventListener("click", () => {
+    removeTask(taskItem, taskText);
+  });
+
+  // Append the clear button to the task item
+  taskItem.appendChild(clearBtn);
+
+  todoList.appendChild(taskItem);
+}
+
+// Remove a task
+function removeTask(taskItem, taskText) {
+  const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  const updatedTasks = tasks.filter((task) => task !== taskText);
+  saveTasks(updatedTasks);
+  todoList.removeChild(taskItem);
+}
+
+// Add a new task
+function addTask() {
+  const taskText = newTaskInput.value;
+  if (taskText.trim() !== "") {
+    createTaskItem(taskText);
+
+    // Save the task to local storage
+    const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    tasks.push(taskText);
+    saveTasks(tasks);
+
+    newTaskInput.value = "";
+  }
+}
+
+// Clear all tasks
+function clearAllTasks() {
+  todoList.innerHTML = "";
+  localStorage.removeItem("tasks");
+}
+
+addTaskBtn.addEventListener("click", addTask);
+clearAllBtn.addEventListener("click", clearAllTasks);
+
+// Load tasks on page load
+document.addEventListener("DOMContentLoaded", loadTasks);
