790f7eb67fe85985fdbae583a9d360a82ccc7c8d
Kelveen Raj <kelveenrajv@gmail.com>
2023-06-25 14:43:50 +0800

    create database

--- data.js
+++ data.js
@@ -1,16 +1,27 @@
 let database = firebase.database();
-let dataRef = database.ref('data');
+let dataRef = database.ref("data");
 
 let newData = {
-  name: 'John Doe',
+  name: "John Doe",
   age: 25,
-  email: 'johndoe@example.com'
+  email: "johndoe@example.com",
 };
-
-dataRef.set(newData)
-  .then(function() {
-    console.log('Data saved successfully.');
+//Creating Data and Store in DB
+dataRef
+  .set(newData)
+  .then(function () {
+    console.log("Data saved successfully.");
   })
-  .catch(function(error) {
-    console.log('Data could not be saved: ' + error);
+  .catch(function (error) {
+    console.log("Data could not be saved: " + error);
   });
+
+function getData() {
+  dataRef.on("value", function (snapshot) {
+    let dbData = snapshot.val();
+    // get the latest data in snapshot form, then show the data
+    console.log(dbData);
+  });
+}
+// run the function after saving data
+getData();
